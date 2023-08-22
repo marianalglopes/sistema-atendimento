6da44712b52a1de561917a7f6f488e2d95172e4f
marianalglopes <mariana.lopes3@estudante.ifms.edu.br>
2023-08-21 22:18:49 -0400

    Selecionando elementos do DOM

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 import "./libs"
+
+//Seleção dos elementos do DOM
+const inputNome = document.querySelector('#input-nome');
+const selectPrioridade = document.querySelector('#select-prioridade');
+const btnIncluir = document.querySelector('#btn-incluir');
+const ulLista = document.querySelector('#ul-lista');
+const displayNome = document.querySelector('#display-nome');
+const btnChamar = document.querySelector('#btn-chamar');
+
+//Array para armazenar os nomes da lista de atendimento
+const listaDeAtendimento = new Array();
