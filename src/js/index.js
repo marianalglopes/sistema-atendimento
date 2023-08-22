b49c56e469769744c9d4213ff019b83f0d0ca7dd
marianalglopes <mariana.lopes3@estudante.ifms.edu.br>
2023-08-21 22:43:55 -0400

    Adição de funções e eventos

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import "./libs"
-
+//Anotação: Hoisting só funciona com 'var' e declaração de função
 //Seleção dos elementos do DOM
 const inputNome = document.querySelector('#input-nome');
 const selectPrioridade = document.querySelector('#select-prioridade');
@@ -10,3 +10,21 @@ const btnChamar = document.querySelector('#btn-chamar');
 
 //Array para armazenar os nomes da lista de atendimento
 const listaDeAtendimento = new Array();
+
+function incluir(){
+//O 'push' coloca um nome dentro da lista. 'value' é uma propriedade
+//Sempre que incluir, ele logo após chama o 'listar'
+    listaDeAtendimento.push(inputNome.value);
+    listar();
+}
+
+function listar(){
+//Fica rodando, pegando o valor da posição e atribuindo na variável
+    for(let nome of listaDeAtendimento){
+        console.log(nome);
+    }
+}
+
+//Eventos
+//Quando houver o click no botão chama a função 'incluir'
+btnIncluir.onclick = incluir
