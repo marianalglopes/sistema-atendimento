b062314bc081742232dc83d520060e6f2a06331d
marianalglopes <mariana.lopes3@estudante.ifms.edu.br>
2023-09-25 14:06:49 -0400

    #1 Incluir atendimento prioritário

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import "./libs"
 //Anotação: Hoisting só funciona com 'var' e declaração de função
 //Seleção dos elementos do DOM
+
 const inputNome = document.querySelector('#input-nome');
 const selectPrioridade = document.querySelector('#select-prioridade');
 const btnIncluir = document.querySelector('#btn-incluir');
@@ -11,24 +12,42 @@ const btnChamar = document.querySelector('#btn-chamar');
 //Array para armazenar os nomes da lista de atendimento
 const listaDeAtendimento = new Array();
 
-function incluir(){
-//O 'push' coloca um nome dentro da lista. 'value' é uma propriedade
-//Sempre que incluir, ele logo após chama o 'listar'
+function incluir() {
+    //O 'push' coloca um nome dentro da lista. 'value' é uma propriedade
+    //Sempre que incluir, ele logo após chama o 'listar'
     listaDeAtendimento.push(inputNome.value);
     listar();
 }
 
 //Fica rodando, pegando o valor da posição e atribuindo na variável
-function listar(){
-    ulLista.innerHTML='';
-    for(let nome of listaDeAtendimento){
-            let li = document.createElement('li')
-            li.classList.add('list-group-item')
-            li.textContent = nome
-            ulLista.appendChild(li)
-        }
+function listar() {
+    ulLista.innerHTML = '';
+    for (let pessoa of listaDeAtendimento) {
+        let li = document.createElement('li')
+        li.classList.add('list-group-item')
+        li.textContent = pessoa.nome
+        ulLista.appendChild(li)
     }
+}
 
 //Eventos
 //Quando houver o click no botão chama a função 'incluir'
-btnIncluir.onclick = incluir
+
+btnIncluir.onclick = function () {
+    let pessoa = {
+        nome: inputNome.value,
+        prioridade: selectPrioridade.value === 'P' ? true : false //A '?' é um operador ternário que permite expressões condicionais de forma concisa
+    }
+    if (selectPrioridade.value === 'P') {
+        pessoa.prioridade = true
+    }
+
+    if (pessoa.prioridade) {
+        listaDeAtendimento.unshift(pessoa)
+    } else {
+        listaDeAtendimento.push(pessoa)
+    }
+
+    listaDeAtendimento.push(inputNome.value)
+    listar()
+}
